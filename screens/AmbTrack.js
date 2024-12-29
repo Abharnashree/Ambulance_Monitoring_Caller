ebab691020f1314bff7e083cf26b3e7d2abebd07
Kuzon2004 <152062286+Kuzon2004@users.noreply.github.com>
2024-12-29 18:26:08 +0530

    Update AmbTrack.js

--- screens/AmbTrack.js
+++ screens/AmbTrack.js
@@ -2,37 +2,37 @@ import React, { useEffect, useState } from 'react';
 import { Card, Text } from 'react-native-paper';
 import MapView, { Marker, UrlTile, Polyline } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
-//import axios from 'axios';
+import axios from 'axios';
 
 const AmbTrack = ({ route }) => {
   const { location } = route.params;
-  const ambulance = { latitude: 37.78825, longitude: -122.4324, time: 5, dist:1.5, speed : 10};
+  const ambulance = { latitude: 13.020078, longitude: 80.224613, time: 5, dist:1.5, speed : 10};
   const [routeCoordinates, setRouteCoordinates] = useState([]);
-  console.log(location.latitude, location.longitude)
-  console.log(ambulance.latitude, ambulance.longitude)
+//   console.log(location.latitude, location.longitude)
+//   console.log(ambulance.latitude, ambulance.longitude)
   
-//   useEffect(() => {
-//     const fetchRoute = async () => {
-//       try {
-//         const response = await axios.get('https://api.openrouteservice.org/v2/directions/driving-car', {
-//           params: {
-//             api_key: '5b3ce3597851110001cf62483361e889f60c484292407670b3cadcb2',
-//             start: `${location.longitude},${location.latitude}`,
-//             end: `${ambulance.longitude},${ambulance.latitude}`,
-//           },
-//         });
-//         const coordinates = response.data.features[0].geometry.coordinates.map(coord => ({
-//           latitude: coord[1],
-//           longitude: coord[0],
-//         }));
-//         setRouteCoordinates(coordinates);
-//       } catch (error) {
-//         console.error('Error fetching route:', error);
-//       }
-//     };
+  useEffect(() => {
+    const fetchRoute = async () => {
+      try {
+        const response = await axios.get('https://api.openrouteservice.org/v2/directions/driving-car', {
+          params: {
+            api_key: '5b3ce3597851110001cf62483361e889f60c484292407670b3cadcb2',
+            start: `${location.longitude},${location.latitude}`,
+            end: `${ambulance.longitude},${ambulance.latitude}`,
+          },
+        });
+        const coordinates = response.data.features[0].geometry.coordinates.map(coord => ({
+          latitude: coord[1],
+          longitude: coord[0],
+        }));
+        setRouteCoordinates(coordinates);
+      } catch (error) {
+        console.error('Error fetching route:', error);
+      }
+    };
 
-//     fetchRoute();
-//   }, [location, ambulance]);
+    fetchRoute();
+  }, [location, ambulance]);
 
   return (
     <View style={styles.container}>
@@ -62,7 +62,7 @@ const AmbTrack = ({ route }) => {
           <Polyline
             coordinates={routeCoordinates}
             strokeWidth={3}
-            strokeColor="hotpink"
+            strokeColor="black"
           />
         )}
       </MapView>
