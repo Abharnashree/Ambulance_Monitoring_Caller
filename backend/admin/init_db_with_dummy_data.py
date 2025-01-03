52e6fb1168fec7557dd067af9583c64942e2bbfc
shibaniselvakumar <shibs0910@gmail.com>
2025-01-03 16:55:47 +0530

    admin api endpoints added

--- backend/admin/init_db_with_dummy_data.py
+++ backend/admin/init_db_with_dummy_data.py
@@ -5,13 +5,13 @@ from schemas import Ambulance, Caller, Order
 def initDB():
   with app.app_context():
     db.create_all()
-    caller1 = Caller("1234567890")
-    caller2 = Caller("0987654321")
-    amb1 = Ambulance(1, "Basic life support")
-    amb2 = Ambulance(2, "Advanced life support")
-    order1 = Order(1, amb1, caller1)
-    order2 = Order(2, amb1, caller2)
-    order3 = Order(3, amb2, caller1)
+    caller1 = Caller("1234567891")
+    caller2 = Caller("1987654321")
+    amb1 = Ambulance(3, "Basic")
+    amb2 = Ambulance(4, "Advanced")
+    order1 = Order(6, amb1, caller1)
+    order2 = Order(7, amb1, caller2)
+    order3 = Order(8, amb2, caller1)
     db.session.add_all([amb1, amb2, caller1, caller2, order1, order2, order3])
     db.session.commit() 
   return "added"
